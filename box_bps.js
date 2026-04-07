e82374d5ef44f1216c84b25d1ab46b2d8c826628
ALLUGOPI <123483481+ALLUGOPI@users.noreply.github.com>
2026-04-07 13:31:32 +0200

    box_bps.js

--- box_bps.js
+++ box_bps.js
@@ -1,53 +1,66 @@
-(function()  {
-	let template = document.createElement("template");
-	template.innerHTML = `
-		<form id="form">
-			<fieldset>
-				<legend>Color Properties</legend>
-				<table>
-					<tr>
-						<td>Color</td>
-						<td><input id="bps_color" type="text" size="10" maxlength="10"></td>
-					</tr>
-				</table>
-				<input type="submit" style="display:none;">
-			</fieldset>
-		</form>
-		<style>
-		:host {
-			display: block;
-			padding: 1em 1em 1em 1em;
-		}
-		</style>
-	`;
+(function() {
+    let template = document.createElement("template");
+    template.innerHTML = `
+        <style>
+            :host {
+                display: block;
+                padding: 15px;
+                font-family: sans-serif;
+            }
+            .prop-container {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                gap: 10px;
+            }
+            label {
+                font-size: 14px;
+                font-weight: bold;
+            }
+            input {
+                cursor: pointer;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+                height: 30px;
+                width: 50px;
+            }
+        </style>
+        <div class="prop-container">
+            <label for="bps_color">Gauge Color</label>
+            <input id="bps_color" type="color">
+        </div>
+    `;
 
-	class BoxBps extends HTMLElement {
-		constructor() {
-			super();
-			this._shadowRoot = this.attachShadow({mode: "open"});
-			this._shadowRoot.appendChild(template.content.cloneNode(true));
-			this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
-		}
+    class BoxBps extends HTMLElement {
+        constructor() {
+            super();
+            this._shadowRoot = this.attachShadow({mode: "open"});
+            this._shadowRoot.appendChild(template.content.cloneNode(true));
+            
+            // Listen for changes and update immediately
+            this._shadowRoot.getElementById("bps_color").addEventListener("input", this._onColorChange.bind(this));
+        }
 
-		_submit(e) {
-			e.preventDefault();
-			this.dispatchEvent(new CustomEvent("propertiesChanged", {
-					detail: {
-						properties: {
-							color: this.color
-						}
-					}
-			}));
-		}
+        _onColorChange(e) {
+            this.dispatchEvent(new CustomEvent("propertiesChanged", {
+                detail: {
+                    properties: {
+                        color: e.target.value
+                    }
+                }
+            }));
+        }
 
-		set color(newColor) {
-			this._shadowRoot.getElementById("bps_color").value = newColor;
-		}
+        // Setter called by SAC when the property changes elsewhere
+        set color(newColor) {
+            this._shadowRoot.getElementById("bps_color").value = newColor;
+        }
 
-		get color() {
-			return this._shadowRoot.getElementById("bps_color").value;
-		}
-	}
+        // Getter called by SAC to retrieve the current value
+        get color() {
+            return this._shadowRoot.getElementById("bps_color").value;
+        }
+    }
 
-	customElements.define("com-demo-box-bps", BoxBps);
+    customElements.define("com-demo-box-bps", BoxBps);
 })();
